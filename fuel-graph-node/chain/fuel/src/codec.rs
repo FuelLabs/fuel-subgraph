9db4499684e4c981271a12499f615781836ec2d9
Salka1988 <emirsalkicart@gmail.com>
2024-02-16 13:51:46 +0100

    add fuel-graph-node

---
+++ fuel-graph-node/chain/fuel/src/codec.rs
@@ -0,0 +1,40 @@
+pub use crate::protobuf::pbcodec::*;
+
+use graph::{
+    blockchain::{Block as BlockchainBlock, BlockPtr},
+    prelude::BlockNumber,
+};
+
+use std::convert::TryFrom;
+
+impl Block {
+    pub fn parent_ptr(&self) -> Option<BlockPtr> {
+        match self.height {
+            0 => None,
+            _ => Some(BlockPtr {
+                hash: self.prev_id.clone().into(),
+                number: self.number().saturating_sub(1),
+            }),
+        }
+    }
+}
+
+impl<'a> From<&'a Block> for BlockPtr {
+    fn from(b: &'a Block) -> BlockPtr {
+        BlockPtr::new(b.id.clone().into(), b.number())
+    }
+}
+
+impl BlockchainBlock for Block {
+    fn ptr(&self) -> BlockPtr {
+        BlockPtr::try_from(self).unwrap()
+    }
+
+    fn parent_ptr(&self) -> Option<BlockPtr> {
+        self.parent_ptr()
+    }
+
+    fn number(&self) -> i32 {
+        BlockNumber::try_from(self.height).unwrap()
+    }
+}

fb18ca949aaff4ce6c24b0999efcaa57baddb4a0
Salka1988 <emirsalkicart@gmail.com>
2024-04-11 18:58:28 +0200

    update extractor

--- firehose/firehose-extract/src/main.rs
+++ firehose/firehose-extract/src/main.rs
@@ -35,7 +35,7 @@ async fn main() -> anyhow::Result<()> {
     loop {
         let page_req = PaginationRequest::<String> {
             cursor,
-            results: 50,
+            results: 42,
             direction: PageDirection::Forward,
         };
         let query = FullBlocksQuery::build(page_req.into());

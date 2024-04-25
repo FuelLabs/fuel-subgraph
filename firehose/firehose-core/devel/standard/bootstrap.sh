734aa05b9f95924270278825ed9b1f99430b4b23
Salka1988 <emirsalkicart@gmail.com>
2024-04-25 22:11:32 +0200

    add docker

--- firehose/firehose-core/devel/standard/bootstrap.sh
+++ firehose/firehose-core/devel/standard/bootstrap.sh
@@ -5,9 +5,10 @@ set -e
 FIREHOSE_EXTRACT_BIN="/app/firehose-extract"
 FIREFUEL_BIN="/app/firecore"
 STORAGE_DIR="/data/storage_dir"
-CHAIN_ID="$1"
 
-COMMON_LIVE_BLOCKS_ADDR="$(hostname -I | awk '{print $1}'):10024"
+CHAIN_ID="${CHAIN_ID:-$1}"
+
+COMMON_LIVE_BLOCKS_ADDR="$(hostname -I | awk '{print $1}')"
 
 if [ -z "$CHAIN_ID" ]; then
 	echo "Usage: $0 CHAIN_ID"
@@ -38,9 +39,14 @@ start:
      reader-node-path: "$FIREHOSE_EXTRACT_BIN"
      reader-node-arguments: $CHAIN_ID $LAST_HEIGHT
 
-     common-live-blocks-addr: "$COMMON_LIVE_BLOCKS_ADDR"
-     reader-node-grpc-listen-addr: "$COMMON_LIVE_BLOCKS_ADDR"
-
+     common-live-blocks-addr: "$COMMON_LIVE_BLOCKS_ADDR:10019"
+     reader-node-grpc-listen-addr: "$COMMON_LIVE_BLOCKS_ADDR:10019"
+#     merger-grpc-listen-addr: "$COMMON_LIVE_BLOCKS_ADDR:10019"
+#     relayer-grpc-listen-addr: :10019
+#
+#     merger-time-between-store-lookups: 5s
+#     merger-time-between-store-pruning: 10s
+#     merger-delete-threads: 10
 END
 
 cd "$STORAGE_DIR"
